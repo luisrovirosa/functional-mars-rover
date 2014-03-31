52810cf6e2bf2a995b70caad6154b28c6827727c
Luis Rovirosa <luisrovirosa@gmail.com>
2014-03-31 19:33:04 +0200

    Refactoring compare function

--- test/rober-spec.js
+++ test/rober-spec.js
@@ -8,21 +8,27 @@ describe('Rober', function() {
 //    var expectedRober = r.newRober(2, 2, 'E');
 //    expect(roberMoved).toBe(expectedRober);
 //  });
+  var rober;
+  beforeEach(function() {
+    rober = r.newRober(0, 0, 'N');
 
+  });
   it('rober with empty instructions has the same position', function() {
-    var rober = r.newRober(0, 0, 'N');
     var roberMoved = r.moveRober(rober, '');
-    expect(JSON.stringify(roberMoved.position)).toBe(JSON.stringify(rober.position));
+
+    expect(equals(roberMoved.position, rober.position)).toBe(true);
 
   });
 
   it('rober with forward the position is not the same as before moving', function() {
-    var rober = r.newRober(0, 0, 'N');
     var roberMoved = r.moveRober(rober, 'F');
-    expect(JSON.stringify(roberMoved.position)).not.toBe(JSON.stringify(rober.position));
+
+    expect(equals(rober.position, roberMoved.position)).not.toBe(true);
 
   });
 
-
-
 });
+
+var equals = function(object1, object2) {
+  return JSON.stringify(object1) === JSON.stringify(object2);
+};
